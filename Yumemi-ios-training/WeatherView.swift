7b87a4bfb1b209559e4c396c115b3da294a19d02
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-04-30 11:13:24 +0900

    レイアウトをstoryboardではなくコードベースで書くための準備をした。

---
+++ Yumemi-ios-training/WeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherView.swift
+//  Yumemi-ios-training
+//
+//  Created by 大塚 周 on 2021/04/30.
+//
+
+import UIKit
+
+class WeatherView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
