9efa7f78739c21a0571cb81c3b67264d83888de7
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-05-07 09:36:21 +0900

    最低気温と最高気温を表示するラベルを追加。二つのラベルを水平方向のStackViewに入れ、それをImageVIewとともに鉛直方向のStackViewに入れている。

--- Yumemi-ios-training/WeatherView.swift
+++ Yumemi-ios-training/WeatherView.swift
@@ -11,13 +11,19 @@ class WeatherView: UIView {
     
     let stackViewForImageViewAndLabels = UIStackView()
     let weatherImageView = UIImageView()
-    
+    let stackViewForLabels = UIStackView()
+    let lowestTemperatureLabel = UILabel()
+    let highestTemparatureLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
+        setupstackViewForImageViewAndLabels()
+        setupstackViewForLabels()
         setupWeatherImage()
         addSubviewConstraints()
+        setupLowestTemperatureLabel()
+        setupHighestTemperatureLabel()
     }
     
     required init?(coder: NSCoder) {
@@ -31,16 +37,46 @@ class WeatherView: UIView {
             stackViewForImageViewAndLabels.centerXAnchor.constraint(equalTo: self.centerXAnchor),
             stackViewForImageViewAndLabels.centerYAnchor.constraint(equalTo: self.centerYAnchor)
         ])
+        
         stackViewForImageViewAndLabels.addArrangedSubview(weatherImageView)
         weatherImageView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
             weatherImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5),
             weatherImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5)
         ])
+        
+        addSubview(stackViewForLabels)
+        stackViewForLabels.addArrangedSubview(lowestTemperatureLabel)
+        stackViewForLabels.addArrangedSubview(highestTemparatureLabel)
+        stackViewForImageViewAndLabels.addArrangedSubview(stackViewForLabels)
+        
+    }
+    
+    private func setupstackViewForImageViewAndLabels() {
+        stackViewForImageViewAndLabels.axis = .vertical
+        stackViewForImageViewAndLabels.alignment = .fill
+        stackViewForImageViewAndLabels.distribution = .fill
+    }
+    
+    private func setupstackViewForLabels() {
+        stackViewForLabels.axis = .horizontal
+        stackViewForLabels.alignment = .fill
+        stackViewForLabels.distribution = .fillEqually
     }
     
     private func setupWeatherImage() {
         weatherImageView.image = UIImage(systemName: "sun.max")
     }
     
+    private func setupLowestTemperatureLabel() {
+        lowestTemperatureLabel.text = "25"
+        lowestTemperatureLabel.textColor = .systemBlue
+        lowestTemperatureLabel.textAlignment = .center
+    }
+    
+    private func setupHighestTemperatureLabel() {
+        highestTemparatureLabel.text = "35"
+        highestTemparatureLabel.textColor = .systemRed
+        highestTemparatureLabel.textAlignment = .center
+    }
 }
