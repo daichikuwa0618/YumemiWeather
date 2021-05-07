0c9c87c832c6260502e692aabadd4b6bffd04082
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-05-07 14:11:15 +0900

    呼び出しAPIをThrows var に変更した。

--- Yumemi-ios-training/Model/WeatherModel.swift
+++ Yumemi-ios-training/Model/WeatherModel.swift
@@ -36,5 +36,3 @@ struct WeatherModel {
         }
     }
 }
-
-
--- Yumemi-ios-training/WeatherModel.swift
+++ Yumemi-ios-training/WeatherModel.swift
@@ -9,6 +9,7 @@ import UIKit
 import YumemiWeather
 
 struct WeatherModel {
+<<<<<<< HEAD
     func reload(imageView: UIImageView, controller: UIViewController) {
         do {
             let weatherString = try YumemiWeather.fetchWeather("{\"area\":\"tokyo\",\"date\":\"2020-04-01T12:00:00+09:00\"}")
@@ -19,6 +20,25 @@ struct WeatherModel {
             let errorAction = UIAlertAction(title: "OK", style: .default)
             errorAlert.addAction(errorAction)
             controller.present(errorAlert, animated: true, completion: nil)
+=======
+    func reload(imageView: UIImageView) {
+        do {
+            let weatherString = try YumemiWeather.fetchWeather(at: "tokyo")
+            switch weatherString {
+            case "sunny":
+                imageView.image = UIImage(named: "sunny")?.withRenderingMode(.alwaysTemplate)
+                imageView.tintColor = .systemRed
+            case "rainy":
+                imageView.image = UIImage(named: "rainy")?.withRenderingMode(.alwaysTemplate)
+                imageView.tintColor = .systemBlue
+            default:
+                imageView.image = UIImage(named: "cloudy")?.withRenderingMode(.alwaysTemplate)
+                imageView.tintColor = .systemGray
+            }
+            
+        } catch {
+            print("error")
+>>>>>>> 0a304f3... 呼び出しAPIをThrows var に変更した。
         }
     }
 }
