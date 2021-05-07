f15fb60812244f6029b216ce224fd535b198e387
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-05-07 09:08:02 +0900

    画面の中心に画像を配置した。 今後その下にラベルを追加するにあたりStackViewを用いるので、ImageViewが追加されたStackViewを配置している。

--- Yumemi-ios-training/WeatherView.swift
+++ Yumemi-ios-training/WeatherView.swift
@@ -9,13 +9,38 @@ import UIKit
 
 class WeatherView: UIView {
     
+    let stackViewForImageViewAndLabels = UIStackView()
+    let weatherImageView = UIImageView()
+    
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
+        setupWeatherImage()
+        addSubviewConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func addSubviewConstraints() {
+        addSubview(stackViewForImageViewAndLabels)
+        stackViewForImageViewAndLabels.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            stackViewForImageViewAndLabels.centerXAnchor.constraint(equalTo: self.centerXAnchor),
+            stackViewForImageViewAndLabels.centerYAnchor.constraint(equalTo: self.centerYAnchor)
+        ])
+        stackViewForImageViewAndLabels.addArrangedSubview(weatherImageView)
+        weatherImageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            weatherImageView.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5),
+            weatherImageView.heightAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.5)
+        ])
+    }
+    
+    private func setupWeatherImage() {
+        weatherImageView.image = UIImage(systemName: "sun.max")
     }
     
 }
