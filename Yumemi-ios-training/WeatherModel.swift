a8ae3da550fb7b1e825acd1e954798dae1db6447
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-05-12 14:38:37 +0900

    do{}のネストを解消した

--- Yumemi-ios-training/WeatherModel.swift
+++ Yumemi-ios-training/WeatherModel.swift
@@ -15,17 +15,22 @@ struct WeatherModel {
         do {
             let weatherJsonString = try YumemiWeather.fetchWeather("{\"area\":\"tokyo\",\"date\":\"2020-04-01T12:00:00+09:00\"}")
             guard let weatherData = weatherJsonString.data(using: String.Encoding.utf8) else { return }
-            do {
-                let items = try JSONSerialization.jsonObject(with: weatherData) as? [String:Any]
-                lowestLabel.text = String(items?["min_temp"] as? Int ?? 0)
-                highestLabel.text = String(items?["max_temp"] as? Int ?? 0)
-                imageView.image = UIImage(named: items?["weather"] as? String ?? "")
-            }
+            changeDisplay(items: try decode(data: weatherData), imageView: imageView, lowestLabel: lowestLabel, highestLabel: highestLabel)
         } catch {
             let errorAlert = UIAlertController(title: "エラー", message: "エラーが発生しました", preferredStyle: .alert)
             let errorAction = UIAlertAction(title: "OK", style: .default)
             errorAlert.addAction(errorAction)
             controller.present(errorAlert, animated: true, completion: nil)
         }
+    }
+    
+    func decode(data: Data) throws -> [String: Any]? {
+        try JSONSerialization.jsonObject(with: data) as? [String:Any]
+    }
+    
+    func changeDisplay(items: [String: Any]?, imageView: UIImageView, lowestLabel: UILabel, highestLabel: UILabel) {
+        lowestLabel.text = String(items?["min_temp"] as? Int ?? 0)
+        highestLabel.text = String(items?["max_temp"] as? Int ?? 0)
+        imageView.image = UIImage(named: items?["weather"] as? String ?? "")
     }
 }
