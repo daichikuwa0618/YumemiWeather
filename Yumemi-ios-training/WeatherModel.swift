0313cf1ad7e530644a76f247f2b2dbfe4e5be8bb
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-05-12 14:28:10 +0900

    Codableを使ったdecodeはSession5の内容なので使わないコーディングに戻した。

--- Yumemi-ios-training/WeatherModel.swift
+++ Yumemi-ios-training/WeatherModel.swift
@@ -8,12 +8,6 @@
 import UIKit
 import YumemiWeather
 
-struct Weather: Codable {
-    let max_temp: Int
-    let min_temp: Int
-    let weather: String
-}
-
 struct WeatherModel {
     let decoder = JSONDecoder()
 
@@ -21,26 +15,17 @@ struct WeatherModel {
         do {
             let weatherJsonString = try YumemiWeather.fetchWeather("{\"area\":\"tokyo\",\"date\":\"2020-04-01T12:00:00+09:00\"}")
             guard let weatherData = weatherJsonString.data(using: String.Encoding.utf8) else { return }
-            try changeDisplay(weatherData: weatherData, imageView: imageView, controller: controller, lowestLabel: lowestLabel, highestLabel: highestLabel)
+            do {
+                let items = try JSONSerialization.jsonObject(with: weatherData) as? [String:Any]
+                lowestLabel.text = String(items?["min_temp"] as? Int ?? 0)
+                highestLabel.text = String(items?["max_temp"] as? Int ?? 0)
+                imageView.image = UIImage(named: items?["weather"] as? String ?? "")
+            }
         } catch {
             let errorAlert = UIAlertController(title: "エラー", message: "エラーが発生しました", preferredStyle: .alert)
             let errorAction = UIAlertAction(title: "OK", style: .default)
             errorAlert.addAction(errorAction)
             controller.present(errorAlert, animated: true, completion: nil)
-        }
-    }
-    
-    func changeDisplay(weatherData: Data, imageView: UIImageView, controller: UIViewController, lowestLabel: UILabel, highestLabel: UILabel) throws {
-        let item = try decoder.decode(Weather.self, from: weatherData)
-        lowestLabel.text = String(item.min_temp)
-        highestLabel.text = String(item.max_temp)
-        switch item.weather {
-        case "sunny":
-            imageView.image = UIImage(named: "sunny")
-        case "cloudy":
-            imageView.image = UIImage(named: "cloudy")
-        default:
-            imageView.image = UIImage(named: "rainy")
         }
     }
 }
