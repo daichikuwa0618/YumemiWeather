8620c9e54bd62a9ebc9be24811eb52110fdd6760
Amane Otsuka <a_otsuka@yumemi.co.jp>
2021-05-21 17:09:27 +0900

    no message

--- Yumemi-ios-training/Model/WeatherModel.swift
+++ Yumemi-ios-training/Model/WeatherModel.swift
@@ -13,24 +13,11 @@ struct WeatherModel {
         do {
             let weatherDataString = try YumemiWeather.fetchWeather("{\"area\": \"tokyo\", \"date\": \"2020-04-01T12:00:00+09:00\" }")
             let weatherData = weatherDataString.data(using: String.Encoding.utf8)!
-<<<<<<< HEAD
             let items = try JSONSerialization.jsonObject(with: weatherData) as! Dictionary<String, Any>
             let weather = Weather(rawValue: items["weather"] as! String)!
             let lowestTemperature = items["min_temp"] as! Int
             let highestTemperature = items["max_temp"] as! Int
             return WeatherViewState(weather: weather, lowestTemperature: lowestTemperature, highestTemperature: highestTemperature)
-=======
-            let weather = Weather(rawValue: weatherString)!
-            
-            switch weather {
-            case .sunny:
-                return WeatherViewState(weather: .sunny)
-            case .cloudy:
-                return WeatherViewState(weather: .cloudy)
-            case .rainy:
-                return WeatherViewState(weather: .rainy)
-            }
->>>>>>> 6f8c957... no message
         } catch {
             return nil
         }
